a897ce9da0c5afa064cb363dabb4ce6679ba2500
Steven Hancock <stevenh512@gmail.com>
2011-04-15 04:26:04 -0700

    Bumped version to 0.0.7.pre

--- lib/accessible_attributes/version.rb
+++ lib/accessible_attributes/version.rb
@@ -2,8 +2,8 @@ module AccessibleAttributes
   module Version
     MAJOR  = 0
     MINOR  = 0
-    PATCH  = 6
-    PRE = nil
+    PATCH  = 7
+    PRE = "pre"
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join(".")
   end
 end
